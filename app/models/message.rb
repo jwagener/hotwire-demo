1abc90b485a030af928ff4e448b5da14224948ee
Johannes Wagener <johannes@wagener.cc>
2021-01-10 15:44:39 +0100

    Add message#broadcast_counts

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :room
   broadcasts_to :room
+  after_create :broadcast_counts
+
+  def broadcast_counts
+    room.broadcast_counts
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,11 @@
 class Room < ApplicationRecord
   has_many :messages, dependent: :destroy
   broadcasts
+
+  def broadcast_counts
+    # TODO how to make these channels secure, so we don't broadcast to unauthorized users?
+    Turbo::StreamsChannel.broadcast_replace_to "total_count", target: "total_count", partial: "rooms/total_count", locals: {text: "#{Message.all.count}"}
+    Turbo::StreamsChannel.broadcast_replace_to "room_#{id}_count", target: "room_#{id}_count", partial: "rooms/room_count", locals: {room: self, text: "#{messages.count}"}
+  end
+
 end
